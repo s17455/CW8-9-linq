6e8974acc634c7aa15b8b58b0428f3dfcf8d3003
Jakub Rusinowicz <jru@moleo.pl>
2020-05-29 23:11:57 +0200

    .

---
+++ LinqConsoleApp/Dept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+        
+        public override string ToString()
+        {
+            return Dname + " (" + Deptno + ")";
+        }
+    }
+}
